5366f44fc15a4020d459a65d56af91e269821d81
Rafal Lesnicki <lesnickirafal@gmail.com>
2021-03-04 08:42:44 +0100

    c

---
+++ src/main/java/pl/javastart/lekcja29zadanie/Lekcja29zadanieApplication.java
@@ -0,0 +1,13 @@
+package pl.javastart.lekcja29zadanie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lekcja29zadanieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lekcja29zadanieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/javastart/lekcja29zadanie/Lekcja29zadanieApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.javastart.lekcja29zadanie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lekcja29zadanieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
